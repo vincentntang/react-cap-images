dc296c1c3d72fe7742c48d17985ceab99d87f88d
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-25 23:24:30 -0400

    Successful API call to endpoint

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React, { Component } from "react";
 import Picture from "./components/Picture";
 import Mesh from "./components/Mesh";
 import "./App.css";
+import axios from "axios";
+require("dotenv").config();
 
 class App extends Component {
+  componentDidMount() {
+    const apiKey = "Bearer " + process.env.REACT_APP_AIRTABLE_KEY;
+    axios
+      .get("https://api.airtable.com/v0/appjyRSLnk3amftj3/Table%201", {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: apiKey
+        }
+      })
+      .then(res => {
+        const images = res.data;
+        console.log(images);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
   render() {
     return (
       <div className="App">
