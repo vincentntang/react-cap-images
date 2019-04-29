cc8437cf1353cf9efcb1fbf0f0c0965955e8f708
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-28 23:47:15 -0400

    First Passing Test Test Case

---
+++ src/test/Mesh.test.js
@@ -0,0 +1,52 @@
+/**
+ * @file Test suite for the Calculator component.
+ */
+
+import React from "react";
+import renderer from "react-test-renderer";
+import { mount, shallow } from "enzyme";
+
+// import Calculator, { getSum } from "../components/calculator";
+
+describe("It should pass", () => {
+  it("should pass", () => {
+    expect(8).toEqual(8);
+  });
+});
+
+// describe("Calculator component", () => {
+//   it("should render snapshot", () => {
+//     const component = renderer.create(<Calculator />);
+
+//     const tree = component.toJSON();
+//     expect(tree).toMatchSnapshot();
+//   });
+
+//   it("should return the correct sum", () => {
+//     const sum = getSum(3, 5);
+//     expect(sum).toEqual(8);
+//   });
+
+//   it("should render required form elements", () => {
+//     const calculator = shallow(<Calculator />);
+
+//     const form = calculator.find("form");
+//     expect(form.length).toBe(1);
+//     expect(form.find("input").length).toBe(2);
+//     expect(form.find("button").length).toBe(1);
+//     expect(form.find("p.result").length).toBe(1);
+//   });
+
+//   it("should display the result on add", () => {
+//     const calculator = mount(<Calculator />);
+
+//     const form = calculator.find("form");
+
+//     form.childAt(0).instance().value = 3;
+//     form.childAt(1).instance().value = 5;
+//     form.find("button").simulate("click");
+
+//     const result = calculator.find(".result");
+//     expect(result.text()).toEqual("8");
+//   });
+// });
---
+++ src/test/setup.js
@@ -0,0 +1,8 @@
+/**
+ * @file Tests setup.
+ */
+
+// import "@babel/polyfill";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
